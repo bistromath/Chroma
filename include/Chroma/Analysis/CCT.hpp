ca84d3aac0db4537b09320913017f349d3b8167a
Nick Foster <bistromath@gmail.com>
2014-07-06 16:51:00 -0700

    Cleanup, add tolerance to CCT calc.

--- include/Chroma/Analysis/CCT.hpp
+++ include/Chroma/Analysis/CCT.hpp
@@ -9,6 +9,6 @@ enum CHROMA_API CCT_method {
     CCT_METHOD_ITERATIVE
 };
 
-float CHROMA_API CCT(const Chroma::xyY &chrom, Chroma::CCT_method method);
+float CHROMA_API CCT(const Chroma::xyY &chrom, Chroma::CCT_method method=CCT_METHOD_MCCAMY, float tol=0.1);
 
 } /* namespace Chroma */
