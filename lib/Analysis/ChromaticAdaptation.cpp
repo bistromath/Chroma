ed2aa8a9e519a1c46359062e7a527b7a0c4fe34a
Nick Foster <bistromath@gmail.com>
2014-07-07 08:41:58 -0700

    Get rid of the Eigen static initializer hack (requires plugin in client applications). Move adaptation vectors into column-major format.

--- lib/Analysis/ChromaticAdaptation.cpp
+++ lib/Analysis/ChromaticAdaptation.cpp
@@ -3,14 +3,17 @@
 #include <eigen3/Eigen/Dense>
 
 /* TODO: There is definitely a better way to do this */
-Chroma::XYZ Chroma::chromatic_adaptation(const Chroma::XYZ &color, const Chroma::XYZ &from, const Chroma::XYZ &to, const Eigen::Matrix3f &adaptation)
+Chroma::XYZ Chroma::chromatic_adaptation(const Chroma::XYZ &color, const Chroma::XYZ &from, const Chroma::XYZ &to, const std::vector<float> &adaptation)
 {
-    Eigen::Vector3f src { from.X, from.Y, from.Z };
-    Eigen::Vector3f dst { to.X, to.Y, to.Z };
-    Eigen::Array3f crd_s = adaptation * src;
-    Eigen::Array3f crd_d = adaptation * dst;
+    Eigen::Vector3f src;
+    src << from.X, from.Y, from.Z;
+    Eigen::Vector3f dst;
+    dst << to.X, to.Y, to.Z;
+    Eigen::Matrix3f adapt_m(adaptation.data());
+    Eigen::Array3f crd_s = adapt_m * src;
+    Eigen::Array3f crd_d = adapt_m * dst;
     Eigen::Vector3f quot = crd_d / crd_s;
-    Eigen::Matrix3f M = adaptation.inverse() * quot.asDiagonal() * adaptation;
+    Eigen::Matrix3f M = adapt_m.inverse() * quot.asDiagonal() * adapt_m;
     Eigen::Vector3f res = M * Eigen::Vector3f(color.X, color.Y, color.Z);
     return Chroma::XYZ(res(0), res(1), res(2));
 }
